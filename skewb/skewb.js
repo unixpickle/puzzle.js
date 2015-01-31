da1faca452fb0c36b9af71708509c3a6bf42be93
Alex Nichol <unixpickle@gmail.com>
2015-01-30 20:33:14 -0500

    wrote porting scripts

--- skewb/skewb.js
+++ skewb/skewb.js
@@ -4,6 +4,10 @@
     this.piece = piece;
     this.orientation = orientation;
   }
+  
+  Corner.prototype.copy = function() {
+    return new Corner(this.piece, this.orientation);
+  };
 
   function Skewb() {
     this.centers = [0, 1, 2, 3, 4, 5];
@@ -11,6 +15,16 @@
     for (var i = 0; i < 8; ++i) {
       this.corners.push(new Corner(i, 0));
     }
+  }
+  
+  Skewb.prototype.copy = function() {
+    var res = Object.create(Skewb.prototype);
+    res.centers = this.centers.slice();
+    res.corners = [];
+    for (var i = 0; i < 8; ++i) {
+      res.corners[i] = this.corners[i].copy();
+    }
+    return res;
   }
 
 })();
