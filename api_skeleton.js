5944dda4f4f9476bcbdb26ca074ab582e5115857
Alex Nichol <unixpickle@gmail.com>
2015-02-04 17:27:49 -0500

    started creating basic build system

---
+++ api_skeleton.js
@@ -0,0 +1,24 @@
+// This is the skeleton of an API.
+(function() {
+
+  var exports;
+  if ('undefined' !== typeof window || 'undefined' !== typeof self) {
+    // Browser or Web Worker.
+    var w = (window || self);
+    if (!w.puzzlejs) {
+      w.puzzlejs = {APINAME: {}};
+    } else if (w.puzzlejs.APINAME) {
+      w.puzzlejs.APINAME = {};
+    }
+    exports = w.puzzlejs.APINAME;
+  } else if ('undefined' !== typeof module) {
+    // Node.js
+    if (!module.exports) {
+      module.exports = {};
+    }
+    exports = module.exports;
+  }
+  
+  APIBODY
+
+})();
