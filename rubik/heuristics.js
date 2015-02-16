42fd20672d840e4d0a3e19423e917fe064c8e21a
Alex Nichol <unixpickle@gmail.com>
2015-02-16 08:18:14 -0500

    implemented EO heuristic

--- rubik/heuristics.js
+++ rubik/heuristics.js
@@ -34,6 +34,42 @@ COHeuristic.prototype.lookup = function(cube) {
   return this.table[encodeCO(cube.corners)];
 };
 
+function EOHeuristic() {
+  this.table = [];
+  
+  // We populate the table a) so every cell is marked invalid, and b) so the
+  // array doesn't become "sparse" under V8.
+  for (var i = 0; i < 2048; ++i) {
+    this.table[i] = -1;
+  }
+}
+
+EOHeuristic.prototype.generate = function() {
+  // Use breadth-first search to generate a heuristic.
+  var nodes = [{state: new Edges(), depth: 0}];
+  var moves = allMoves();
+  while (nodes.length > 0) {
+    var node = nodes[0];
+    nodes.splice(0, 1);
+    
+    var idx = encodeEO(node.state);
+    if (this.table[idx] >= 0) {
+      continue
+    }
+    this.table[idx] = node.depth;
+    
+    for (var i = 0, len = moves.length; i < len; ++i) {
+      var newState = node.state.copy();
+      newState.move(moves[i]);
+      nodes.push({state: newState, depth: node.depth+1});
+    }
+  }
+};
+
+EOHeuristic.prototype.lookup = function(cube) {
+  return this.table[encodeEO(cube.edges)];
+};
+
 function encodeCO(corners) {
   var res = 0;
   var mul = 1;
@@ -44,4 +80,14 @@ function encodeCO(corners) {
   return res;
 }
 
+function encodeEO(edges) {
+  var res = 0;
+  for (var i = 0; i < 11; ++i) {
+    var orientation = (edges.edges[i].flip ? 1 : 0);
+    res += orientation << i;
+  }
+  return res;
+}
+
 exports.COHeuristic = COHeuristic;
+exports.EOHeuristic = EOHeuristic;
--- rubik_test/heuristic_test.js
+++ rubik_test/heuristic_test.js
@@ -9,6 +9,16 @@ function benchmarkCOHeuristic() {
   console.log('Benchmark: ' + duration/10 + ' ms/COHeuristic.');
 }
 
+function benchmarkEOHeuristic() {
+  var start = new Date().getTime();
+  for (var i = 0; i < 10; ++i) {
+    new rubik.EOHeuristic().generate();
+  }
+  var duration = new Date().getTime() - start;
+  console.log('Benchmark: ' + duration/10 + ' ms/EOHeuristic.');
+}
+
 // Run tests.
 benchmarkCOHeuristic();
+benchmarkEOHeuristic();
 console.log('PASS');
