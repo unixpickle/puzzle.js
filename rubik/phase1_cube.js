f7eec0f467f22433264c1b64b2c785dd244a2711
Alex Nichol <unixpickle@gmail.com>
2015-04-05 15:40:46 -0400

    finished draft of Phase1Cube and Phase1Moves

--- rubik/phase1_cube.js
+++ rubik/phase1_cube.js
@@ -36,20 +36,36 @@ var zMoveTranslation = [3, 2, 0, 1, 4, 5, 9, 8, 6, 7, 10, 11, 15, 14, 12, 13,
 // The FB edge orientation can be used for both Y and X phase-1 goals, and the
 // UD edge orientation can be used for the Z phase-1 goal. Thus, no RL edge
 // orientations are needed.
-function Phase1Cube() {
-  // These are the initial corner orientations.
-  this.xCO = 1093;
-  this.yCO = 1093;
-  this.zCO = 1093;
+function Phase1Cube(cc) {
+  if ('undefined' !== typeof cc) {
+    var xzCO = encodeXZCO(cc.corners);
+    
+    this.xCO = xzCO[0];
+    this.yCO = encodeCO(cc.corners);
+    this.zCO = xzCO[1];
+    
+    this.fbEO = encodeEO(cc.edges);
+    this.udEO = encodeUDEO(cc.edges);
+    
+    var msSlice = encodeMSSlice(cc.edges);
+    this.mSlice = msSlice[0];
+    this.eSlice = encodeESlice(cc.edges);
+    this.sSlice = msSlice[1];
+  } else {
+    // These are the initial corner orientations.
+    this.xCO = 1093;
+    this.yCO = 1093;
+    this.zCO = 1093;
   
-  // These are the initial edge orientations.
-  this.fbEO = 0;
-  this.udEO = 0;
+    // These are the initial edge orientations.
+    this.fbEO = 0;
+    this.udEO = 0;
   
-  // These are the initial slice permutations.
-  this.mSlice = 220;
-  this.eSlice = 220;
-  this.sSlice = 220;
+    // These are the initial slice permutations.
+    this.mSlice = 220;
+    this.eSlice = 220;
+    this.sSlice = 220;
+  }
 }
 
 // anySolved returns true if the phase-1 state is solved along any axis.
@@ -62,6 +78,27 @@ Phase1Cube.prototype.anySolved = function() {
     return true;
   }
   return false;
+};
+
+// move applies a move to the Phase1Cube.
+Phase1Cube.prototype.move = function(move, table) {
+  var m = move.number;
+  
+  // Apply the move to the y-axis cube.
+  this.yCO = table.co[this.yCO][m];
+  this.fbEO = table.eo[this.fbEO][m];
+  this.eSlice = table.slice[this.eSlice][m];
+  
+  // Apply the move to the z-axis cube.
+  var zMove = zMoveTranslation[m];
+  this.zCO = table.co[this.zCO][zMove];
+  this.udEO = table.eo[this.udEO][zMove];
+  this.sSlice = table.slice[this.sSlice][zMove];
+  
+  // Apply the move to the x-axis cube.
+  var xMove = xMoveTranslation[m];
+  this.xCO = table.co[this.xCO][xMove];
+  this.mSlice = table.slice[this.mSlice][xMove];
 };
 
 // solved returns an array with three booleans, [x, y, z], which indicates
@@ -129,7 +166,7 @@ Phase1Cube.prototype.xEO = function() {
 // phase-1 goal. Moves much be translated for the X-oriented and Z-oriented
 // goals.
 function Phase1Moves() {
-  this.eSlice = [];
+  this.slice = [];
   this.eo = [];
   this.co = [];
   
@@ -147,8 +184,23 @@ Phase1Moves.prototype._generateCO = function() {
     }
   }
   
-  // TODO: this.
-  throw new Error('Not yet implemented.');
+  for (var i = 0; i < 2187; ++i) {
+    var corners = decodeCO(i);
+    for (var m = 0; m < 18; ++m) {
+      if (this.co[i][m] >= 0) {
+        continue;
+      }
+      
+      // Set the end state in the table.
+      var aCase = corners.copy();
+      aCase.move(new Move(m));
+      var endState = encodeCO(aCase);
+      this.co[i][m] = endState;
+      
+      // Set the inverse in the table.
+      this.co[endState][new Move(m).inverse().number] = i;
+    }
+  }
 };
 
 Phase1Moves.prototype._generateEO = function() {
@@ -160,16 +212,31 @@ Phase1Moves.prototype._generateEO = function() {
     }
   }
   
-  // TODO: this.
-  throw new Error('Not yet implemented.');
+  for (var i = 0; i < 2048; ++i) {
+    var edges = decodeEO(i);
+    for (var m = 0; m < 18; ++m) {
+      if (this.eo[i][m] >= 0) {
+        continue;
+      }
+      
+      // Set the end state in the table.
+      var aCase = edges.copy();
+      aCase.move(new Move(m));
+      var endState = encodeEO(aCase);
+      this.eo[i][m] = endState;
+      
+      // Set the inverse in the table.
+      this.eo[endState][new Move(m).inverse().number] = i;
+    }
+  }
 };
 
 Phase1Moves.prototype._generateESlice = function() {
   // Set all elements to -1.
   for (var i = 0; i < 495; ++i) {
-    this.eSlice[i] = [];
+    this.slice[i] = [];
     for (var j = 0; j < 18; ++j) {
-      this.eSlice[i][j] = -1;
+      this.slice[i][j] = -1;
     }
   }
   
@@ -187,7 +254,7 @@ Phase1Moves.prototype._generateESlice = function() {
           state.edges[y].piece = -1;
           state.edges[z].piece = -1;
           for (var m = 0; m < 18; ++m) {
-            if (this.eSlice[sliceCase][m] >= 0) {
+            if (this.slice[sliceCase][m] >= 0) {
               continue;
             }
             
@@ -195,10 +262,10 @@ Phase1Moves.prototype._generateESlice = function() {
             var aCase = state.copy();
             aCase.move(new Move(m));
             var encoded = encodeBogusSlice(aCase);
-            this.eSlice[sliceCase][m] = encoded;
+            this.slice[sliceCase][m] = encoded;
             
             // Set the inverse in the table.
-            this.eSlice[encoded][new Move(m).inverse().number] = sliceCase;
+            this.slice[encoded][new Move(m).inverse().number] = sliceCase;
           }
           ++sliceCase;
         }
@@ -207,12 +274,221 @@ Phase1Moves.prototype._generateESlice = function() {
   }
 };
 
+// decodeCO generates Corners which represent a given CO case.
+function decodeCO(co) {
+  var c = new Corners();
+  
+  // Compute the orientations of the first 7 corners.
+  var scaler = 1;
+  for (var x = 0; x < 7; ++x) {
+    c.corners[x].orientation = Math.floor(co / scaler) % 3;
+    scaler *= 3;
+  }
+  
+  // Compute the last corner's orientation.
+  // The way this works is based on the fact that a sune combo which twists two
+  // adjacent corners is all that is necessary to generate any corner
+  // orientation case.
+  var ordering = [0, 1, 5, 4, 6, 2, 3, 7];
+  var orientations = [];
+  for (var i = 0; i < 8; ++i) {
+    orientations[i] = c.corners[ordering[i]].orientation;
+  }
+  for (var i = 0; i < 7; ++i) {
+    var thisOrientation = orientations[i];
+    var nextOrientation = orientations[i+1];
+    // Twist thisOrientation to be solved, affecting the next corner in the
+    // sequence.
+    if (thisOrientation === 2) {
+      // y -> x, x -> z, z -> y
+      orientations[i+1] = (nextOrientation + 2) % 3;
+    } else if (thisOrientation === 0) {
+      // z -> x, x -> y, y -> z
+      orientations[i+1] = (nextOrientation + 1) % 3;
+    }
+  }
+  // The twist of the last corner is the inverse of what it should be in the
+  // scramble.
+  if (orientations[7] === 0) {
+    c.corners[7].orientation = 2;
+  } else if (orientations[7] === 2) {
+    c.corners[7].orientation = 0;
+  }
+  
+  return c;
+}
+
+// decodeEO generates Edges which represent a given EO case.
+function decodeEO(eo) {
+  var edges = new Edges();
+  var parity = false;
+  for (var x = 0; x < 11; ++x) {
+    if ((eo & (1 << x)) !== 0) {
+      parity = !parity;
+      edges.edges[x].flip = true;
+    }
+  }
+  edges.edges[11].flip = parity;
+  return edges;
+}
+
+// encodeBogusSlice encodes a slice permutation case, treating pieces with
+// values of "-1" as E slice edges.
 function encodeBogusSlice(edges) {
   var list = [];
   for (var i = 0; i < 12; ++i) {
     list[i] = (edges.edges[i].piece === -1);
   }
   return encodeChoose(list);
+}
+
+// encodeCO encodes the CO case of a given set of Corners.
+function encodeCO(c) {
+  var res = 0;
+  var scaler = 1;
+  for (var i = 0; i < 7; ++i) {
+    res += scaler * c.corners[i].orientation;
+    scaler *= 3;
+  }
+  return res;
+}
+
+// encodeEO encodes the EO case of a given set of Edges.
+function encodeEO(e) {
+  var res = 0;
+  for (var i = 0; i < 11; ++i) {
+    if (e.edges[i].flip) {
+      res |= (1 << i);
+    }
+  }
+  return res;
+}
+
+// encodeESlice encodes the E slice of a given set of Edges.
+function encodeESlice(edges) {
+  var list = [];
+  for (var i = 0; i < 12; ++i) {
+    var piece = edges.edges[i].piece;
+    if (piece === 1 || piece === 3 || piece === 7 || piece === 9) {
+      list[i] = true;
+    } else {
+      list[i] = false;
+    }
+  }
+  return encodeChoose(list);
+}
+
+// encodeMSSlice encodes the M and S slices of Edges.
+function encodeMSSlice(edges) {
+  var mChoice = [];
+  var sChoice = [];
+  for (var i = 0; i < 12; ++i) {
+    var idx = xEdgeIndices[i];
+    var p = edges.edges[idx].piece;
+    if (p === 0 || p === 2 || p === 6 || p === 8) {
+      mChoice[i] = true;
+    } else {
+      mChoice[i] = false;
+    }
+  }
+  for (var i = 0; i < 12; ++i) {
+    var idx = zEdgeIndices[i];
+    var p = edges.edges[idx].piece;
+    if (p === 4 || p === 5 || p === 10 || p === 11) {
+      sChoice[i] = true;
+    } else {
+      sChoice[i] = false;
+    }
+  }
+  return [encodeChoose(mChoice), encodeChoose(sChoice)];
+}
+
+// encodeUDEO encodes the UD EO case of Edges.
+function encodeUDEO(edges) {
+  var res = 0;
+  for (var i = 0; i < 11; ++i) {
+    var idx = zEdgeIndices[i];
+    var edge = edges.edges[idx];
+    var flip = edge.flip;
+    var p = edge.piece
+    if (p === 0 || p === 2 || p === 6 || p === 8) {
+      // This is an M slice edge piece, so it changes orientation if it
+      // was on the S slice or the E slice.
+      if (idx !== 0 && idx !== 2 && idx !== 6 && idx !== 8) {
+        flip = !flip;
+      }
+    } else {
+      // This is an E or S slice edge, so it changes orientation if it
+      // was on the M slice.
+      if (idx === 0 || idx === 2 || idx === 6 || idx === 8) {
+        flip = !flip
+      }
+    }
+    if (flip) {
+      res |= 1 << i;
+    }
+  }
+  return res;
+}
+
+// encodeXZCO encodes the CO of Corners on the X and Z axes.
+function encodeXZCO(corners) {
+  var x = [];
+  var z = [];
+  var xVal = 0;
+  var yVal = 0;
+  
+  // For each corner, find the direction of the x and z stickers.
+  for (var i = 0; i < 8; ++i) {
+    var corner = corners.corners[i];
+    
+    // If the corner was in its original slot, here's what the directions
+    // would be.
+    var o = corner.orientation;
+    if (o === 0) {
+      x[i] = 2;
+      z[i] = 1;
+    } else if (o === 1) {
+      x[i] = 0;
+      z[i] = 2;
+    } else {
+      x[i] = 1;
+      z[i] = 0;
+    }
+
+    // If it takes an odd number of quarter turns to move the corner back to
+    // its original slot, swap x and z.
+    var d = (corner.piece ^ i) & 7;
+    if (d === 1 || d === 2 || d === 4 || d === 7) {
+      var oldX = x[i];
+      x[i] = z[i];
+      z[i] = oldX;
+    }
+  }
+  
+  // Add the information together to generate the final values.
+  var scaler = 1;
+  for (var i = 0; i < 7; ++i) {
+    var xDirection = x[xCornerIndices[i]];
+    if (xDirection === 1) {
+      xDirection = 0;
+    } else if (xDirection === 0) {
+      xDirection = 1;
+    }
+    xVal += scaler * xDirection;
+    
+    var zDirection = z[zCornerIndices[i]];
+    if (zDirection === 1) {
+      zDirection = 2;
+    } else if (zDirection === 2) {
+      zDirection = 1;
+    }
+    zVal += scaler * zDirection;
+    
+    scaler *= 3;
+  }
+  
+  return [xVal, zVal];
 }
 
 exports.Phase1Cube = Phase1Cube;
