1c9a8949903da23845a158f1a4c7e9853326c4b2
Alex Nichol <unixpickle@gmail.com>
2015-04-08 18:08:51 -0400

    benchmark xEO and Phase1Heuristic.lowerBound()

--- rubik/bench/phase1_cube_bench.js
+++ rubik/bench/phase1_cube_bench.js
@@ -11,7 +11,7 @@ function benchmarkPhase1Conversion() {
   }
   
   // Run a bunch of conversions.
-  var count = 1000000;
+  var count = 300000;
   var start = new Date().getTime();
   for (var i = 0; i < count; ++i) {
     new rubik.Phase1Cube(cube);
@@ -45,6 +45,27 @@ function benchmarkPhase1Moves() {
   console.log('Benchmark: ' + Math.ceil(duration/count) + ' ms/Phase1Moves');
 }
 
+function benchmarkPhase1XEO() {
+  var count = 10000000;
+  
+  // Generate a semi-scrambled cube to test with. The cube itself doesn't matter
+  // too much, I just don't want it to be solved.
+  var cubieCube = new rubik.CubieCube();
+  for (var i = 0; i < 6; ++i) {
+    cubieCube.move(new rubik.Move(i));
+  }
+  var cube = new rubik.Phase1Cube(cubieCube);
+  
+  var start = new Date().getTime();
+  for (var i = 0; i < count; ++i) {
+    cube.xEO();
+  }
+  var duration = new Date().getTime() - start;
+  
+  console.log('Benchmark: ' + Math.ceil(1000000*duration/count) + ' ns/xEO');
+}
+
 benchmarkPhase1Conversion();
 benchmarkPhase1CubeMove();
 benchmarkPhase1Moves();
+benchmarkPhase1XEO();
--- rubik/bench/phase1_heuristic_bench.js
+++ rubik/bench/phase1_heuristic_bench.js
@@ -11,4 +11,26 @@ function benchmarkHeuristic() {
   console.log('Benchmark: ' + Math.ceil(duration/count) + ' ms/Phase1Heuristic');
 }
 
+function benchmarkLowerBound() {
+  var count = 10000000;
+  var moves = new rubik.Phase1Moves();
+  var heuristic = new rubik.Phase1Heuristic(moves);
+  
+  // Generate a semi-scrambled cube to test with.
+  var cube = new rubik.Phase1Cube();
+  for (var i = 0; i < 6; ++i) {
+    cube.move(new rubik.Move(i), moves);
+  }
+  
+  var start = new Date().getTime();
+  for (var i = 0; i < count; ++i) {
+    heuristic.lowerBound(cube);
+  }
+  var duration = new Date().getTime() - start;
+  
+  console.log('Benchmark: ' + Math.ceil(1000000*duration/count) +
+    ' ns/lowerBound');
+}
+
 benchmarkHeuristic();
+benchmarkLowerBound();
