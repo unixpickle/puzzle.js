0c7dae203904b6f3217b315a23e4f848a7e5fac0
Alex Nichol <unixpickle@gmail.com>
2015-04-08 09:35:03 -0400

    drafted phase1 solver

---
+++ rubik/phase1_solver.js
@@ -0,0 +1,56 @@
+function Solver(heuristic, moves, cb, deadline) {
+  this.heuristic = heuristic;
+  this.moves = moves;
+  this.cb = cb;
+  this.deadline = deadline;
+}
+
+Solver.prototype.solve = function(cube, preMoves, depth, lastFace) {
+  if (depth === 0) {
+    if (cube.anySolved()) {
+      if (this._expired()) {
+        return false;
+      }
+      return this.cb(preMoves.slice());
+    }
+    return true;
+  } else if (this.heuristic.lowerBound(cube) > depth) {
+    return true;
+  }
+
+  for (var i = 0; i < 18; ++i) {
+    var move = new Move(i);
+    if (move.face() === lastFace) {
+      continue;
+    }
+    var newCube = cube.copy();
+    newCube.move(move);
+    preMoves.push(move);
+    if (!this.solve(newCube, preMoves, depth-1, move.face())) {
+      return false;
+    }
+    preMoves.pop();
+    if (depth >= 7 && this._expired()) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+Solver.prototype._expired = function() {
+  return new Date().getTime() > this.deadline;
+};
+
+function solvePhase1(cube, heuristic, moves, cb, timeout) {
+  var solver = new Solver(heuristic, moves, cb, new Date().getTime() + timeout);
+  var depth = 0;
+  while (true) {
+    if (!solver.search(cube, [], depth, 0)) {
+      return;
+    }
+    ++depth;
+  }
+}
+
+exports.solvePhase1 = solvePhase1;
