426d8e202ece8f0c1260067ac3d848f8df07ee33
Alex Nichol <unixpickle@gmail.com>
2015-04-14 17:56:17 -0400

    implemented full Phase2CornerSym tests+benches

--- rubik/bench/phase2_symmetry_bench.js
+++ rubik/bench/phase2_symmetry_bench.js
@@ -2,6 +2,34 @@ var rubik = require('../../build/rubik.js');
 var perms = require('../../build/perms.js');
 var bench = require('../../bench.js');
 
+function benchmarkPhase2CornerSym() {
+  var perm8 = perms.allPerms(8);
+  bench('Phase2CornerSym', function(n) {
+    while (n--) {
+      new rubik.Phase2CornerSym(perm8);
+    }
+  });
+}
+
+function benchmarkPhase2CornerSymMove() {
+  // Apply all moves to a bunch of random symmetry coordinates.
+  var states = [9634, 39604, 19667, 40733, 3187, 10589, 27264, 43833, 581,
+    6282, 21757, 2189, 35665, 9221, 40164, 23510, 40687, 36973, 12676, 8060,
+    25099, 10331, 21511, 3993, 44060, 22141, 28300, 3368, 35519, 27598, 17737,
+    11378, 24311, 30289, 37587, 13416, 25342, 39266, 43774, 32655, 15217, 4445,
+    2545, 34688, 29912, 2667, 1503, 15434, 38614, 11406];
+  var sym = new rubik.Phase2CornerSym(perms.allPerms(8));
+  bench('Phase2CornerSym.move', states.length*10, function(n) {
+    for (var i = 0; i < n/(states.length*10); ++i) {
+      for (var s = 0, len = states.length; s < len; ++s) {
+        for (var m = 0; m < 10; ++m) {
+          sym.move(states[s], m);
+        }
+      }
+    }
+  });
+}
+
 function benchmarkPhase2EdgeSym() {
   var perm8 = perms.allPerms(8);
   bench('Phase2EdgeSym', function(n) {
@@ -19,7 +47,7 @@ function benchmarkPhase2EdgeSymMove() {
     11378, 24311, 30289, 37587, 13416, 25342, 39266, 43774, 32655, 15217, 4445,
     2545, 34688, 29912, 2667, 1503, 15434, 38614, 11406];
   var sym = new rubik.Phase2EdgeSym(perms.allPerms(8));
-  bench('move', states.length*10, function(n) {
+  bench('Phase2EdgeSym.move', states.length*10, function(n) {
     for (var i = 0; i < n/(states.length*10); ++i) {
       for (var s = 0, len = states.length; s < len; ++s) {
         for (var m = 0; m < 10; ++m) {
@@ -30,5 +58,7 @@ function benchmarkPhase2EdgeSymMove() {
   });
 }
 
+benchmarkPhase2CornerSym();
+benchmarkPhase2CornerSymMove();
 benchmarkPhase2EdgeSym();
 benchmarkPhase2EdgeSymMove();
--- rubik/test/phase2_symmetry_test.js
+++ rubik/test/phase2_symmetry_test.js
@@ -2,6 +2,39 @@ var rubik = require('../../build/rubik.js');
 var perms = require('../../build/perms.js');
 var assert = require('assert');
 
+function testP2CornerSymmetryPermute() {
+  var applied = [
+    // y0, y1, y2, y3
+    [0, 1, 2, 3, 4, 5, 6, 7],
+    [4, 0, 6, 2, 5, 1, 7, 3],
+    [5, 4, 7, 6, 1, 0, 3, 2],
+    [1, 5, 3, 7, 0, 4, 2, 6],
+    // LR*y0, LR*y1, LR*y2, LR*y3
+    [1, 0, 3, 2, 5, 4, 7, 6],
+    [0, 4, 2, 6, 1, 5, 3, 7],
+    [4, 5, 6, 7, 0, 1, 2, 3],
+    [5, 1, 7, 3, 4, 0, 6, 2],
+    // UD*y0, UD*y1, UD*y2, UD*y3
+    [2, 3, 0, 1, 6, 7, 4, 5],
+    [6, 2, 4, 0, 7, 3, 5, 1],
+    [7, 6, 5, 4, 3, 2, 1, 0],
+    [3, 7, 1, 5, 2, 6, 0, 4],
+    // UD*LR*y0, UD*LR*y1, UD*LR*y2, UD*LR*y3
+    [3, 2, 1, 0, 7, 6, 5, 4],
+    [2, 6, 0, 4, 3, 7, 1, 5],
+    [6, 7, 4, 5, 2, 3, 0, 1],
+    [7, 3, 5, 1, 6, 2, 4, 0]
+  ];
+  
+  var identity = applied[0];
+  for (var i = 0; i < 0x10; ++i) {
+    var sym = identity.slice();
+    rubik.p2CornerSymmetryPermute(i, sym);
+    assert.deepEqual(sym, applied[i], "Invalid result for symmetry " + i +
+      ": " + sym.join(', '));
+  }
+}
+
 function testP2EdgeSymmetryPermute() {
   var applied = [
     // y0, y1, y2, y3
@@ -33,6 +66,54 @@ function testP2EdgeSymmetryPermute() {
     assert.deepEqual(sym, applied[i], "Invalid result for symmetry " + i +
       ": " + sym.join(', '));
   }
+}
+
+function testPhase2CornerSym() {
+  var sym = new rubik.Phase2CornerSym(perms.allPerms(8));
+  
+  // Make sure there are exactly 2768 unique cases (up to symmetry).
+  var caseCount = 0;
+  var gotten = {};
+  for (var i = 0; i < 40320; ++i) {
+    var s = sym.rawToSym(i) >> 4;
+    if (!gotten[s]) {
+      gotten[s] = true;
+      ++caseCount;
+    }
+  }
+  assert.equal(caseCount, 2768, "invalid number of unique cases: " + caseCount);
+}
+
+function testPhase2CornerSymMove() {
+  var sym = new rubik.Phase2CornerSym(perms.allPerms(8));
+  
+  // Apply the completely random moves D' D' B2 B2 D D2 D2 D B2 U D2 B2 L2 U'
+  // D' D U' R2 L2 U2 U2 U2 U D L2 U2 D' U' L2 U2 D2 D2 U D2 D F2 L2 F2 U' L2
+  // U' D U2 D D2 F2 U' F2 U D.
+  // This case can be created in 12 moves: R2 F2 U R2 U B2 D F2 D2 F2 U' R2.
+  var moves = [8, 8, 1, 1, 7, 9, 9, 7, 1, 4, 9, 1, 3, 5, 8, 7, 5, 2, 3, 6, 6,
+    6, 4, 7, 3, 6, 8, 5, 3, 6, 9, 9, 4, 9, 7, 0, 3, 0, 5, 3, 5, 7, 6, 7, 9, 0,
+    5, 0, 4, 7];
+  
+  // Apply all the moves to the solved state.
+  var state = 0;
+  for (var i = 0; i < moves.length; ++i) {
+    state = sym.move(state, moves[i]);
+  }
+  
+  // Make sure the symmetry coordinate represents the correct permutation.
+  var perm = null;
+  for (var i = 0; i < 40320; ++i) {
+    var s = sym.rawToSym(i);
+    if ((s >>> 4) === (state >>> 4)) {
+      assert.equal(s & 0xf, 0);
+      perm = perms.allPerms(8)[i];
+      break;
+    }
+  }
+  assert.notEqual(perm, null);
+  perm = rubik.p2CornerSymmetryConj(state & 0xf, perm);
+  assert.deepEqual(perm, [7, 4, 0, 1, 3, 2, 5, 6]);
 }
 
 function testPhase2EdgeSym() {
@@ -83,7 +164,10 @@ function testPhase2EdgeSymMove() {
   assert.deepEqual(perm, [3, 6, 5, 4, 1, 0, 7, 2]);
 }
 
+testP2CornerSymmetryPermute();
 testP2EdgeSymmetryPermute();
+testPhase2CornerSym();
+testPhase2CornerSymMove();
 testPhase2EdgeSym();
 testPhase2EdgeSymMove();
 console.log('PASS');
