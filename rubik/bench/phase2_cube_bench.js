5faf773de5c5750238dc7dd26bbbd6cca0741583
Alex Nichol <unixpickle@gmail.com>
2015-04-16 17:54:52 -0400

    benchmark p1->p2 conversion

--- rubik/bench/phase2_cube_bench.js
+++ rubik/bench/phase2_cube_bench.js
@@ -1,6 +1,27 @@
 var rubik = require('../../build/rubik.js');
 var bench = require('../../bench.js');
 
+function benchmarkConversion() {
+  var coords = new rubik.Phase2Coords();
+  var cubieCube = new rubik.CubieCube();
+  
+  var scramble = rubik.parseMoves("R2 U' D' L2 B2 D' R2 F2 U2 D' L2");
+  var cubes = [];
+  for (var i = 0; i < scramble.length; ++i) {
+    cubieCube.move(scramble[i]);
+    cubes[i] = cubieCube.copy();
+  }
+  
+  bench('convertCubieToPhase2', cubes.length, function(count) {
+    var len = cubes.length;
+    for (var i = 0; i < count/len; ++i) {
+      for (var j = 0; j < len; ++j) {
+        rubik.convertCubieToPhase2(cubes[j], 1, coords);
+      }
+    }
+  });
+}
+
 function benchmarkPhase2CubeMove() {
   var coords = new rubik.Phase2Coords();
   var cube = new rubik.Phase2Cube();
@@ -19,5 +40,6 @@ function benchmarkPhase2Coords() {
   });
 }
 
+benchmarkConversion();
 benchmarkPhase2CubeMove();
 benchmarkPhase2Coords();
