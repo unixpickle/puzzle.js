5fef0740b7203299b0a485e70cf5ea7af73ef535
Alex Nichol <unixpickle@gmail.com>
2015-08-09 14:47:39 -0400

    fixed megaminx scrambles using D after U

--- megaminx/moves.js
+++ megaminx/moves.js
@@ -2,8 +2,7 @@ function pochmannScramble(length) {
   var lines = [];
   for (var i = 0; i < Math.ceil(length / 11); ++i) {
     var moves = [];
-    var offset = Math.floor(Math.random() * 2);
-    for (var j = offset; j < 10+offset; ++j) {
+    for (var j = 0; j < 10; ++j) {
       var face = (j & 1) === 0 ? 'R' : 'D';
       var direction = Math.random() < 0.5 ? '--' : '++';
       moves.push(face + direction);
