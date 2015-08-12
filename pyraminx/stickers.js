bd26e327d12f794f869650dea6029e1fd810855f
Alex Nichol <unixpickle@gmail.com>
2015-08-12 13:41:11 -0400

    pyraminx: finished draft of Stickers

--- pyraminx/stickers.js
+++ pyraminx/stickers.js
@@ -25,11 +25,66 @@ Stickers.prototype.move = function(stickerMove) {
   }
 };
 
-StickerMove.prototype._applyMoveToNonTip = function(stickerMove) {
-  // TODO: this
+Stickers.prototype._applyMoveToNonTip = function(stickerMove) {
+  var front = 0;
+  var bottom = 9;
+  var right = 18;
+  var left = 27;
+  
+  if (stickerMove.clockwise) {
+    switch (stickerMove.corner) {
+    case 0: // R
+      this._threeCycle(front+7, right+5, bottom+3);
+      this._threeCycle(front+6, right+1, bottom+7);
+      this._threeCycle(bottom+2, front+3, right+6);
+      break;
+    case 1: // L
+      this._threeCycle(front+5, bottom+1, left+7);
+      this._threeCycle(left+1, bottom+2, left+6);
+      this._threeCycle(left+6, bottom+5, left+3);
+      break;
+    case 2: // U
+      this._threeCycle(front+2, left+2, right+2);
+      this._threeCycle(front+1, left+1, right+1);
+      this._threeCycle(front+3, left+3, right+3);
+      break;
+    case 3: // B
+      this._threeCycle(left+5, bottom+6, right+7);
+      this._threeCycle(left+1, bottom+5, right+6);
+      this._threeCycle(right+3, bottom+7, left+6);
+      break;
+    default:
+      throw new Error('invalid corner: ' + stickerMove.corner);
+    }
+  } else {
+    switch (stickerMove.corner) {
+    case 0: // R'
+      this._reverseThreeCycle(front+7, right+5, bottom+3);
+      this._reverseThreeCycle(front+6, right+1, bottom+7);
+      this._reverseThreeCycle(bottom+2, front+3, right+6);
+      break;
+    case 1: // L'
+      this._reverseThreeCycle(front+5, bottom+1, left+7);
+      this._reverseThreeCycle(left+1, bottom+2, left+6);
+      this._reverseThreeCycle(left+6, bottom+5, left+3);
+      break;
+    case 2: // U'
+      this._reverseThreeCycle(front+2, left+2, right+2);
+      this._reverseThreeCycle(front+1, left+1, right+1);
+      this._reverseThreeCycle(front+3, left+3, right+3);
+      break;
+    case 3: // B'
+      this._reverseThreeCycle(left+5, bottom+6, right+7);
+      this._reverseThreeCycle(left+1, bottom+5, right+6);
+      this._reverseThreeCycle(right+3, bottom+7, left+6);
+      break;
+    default:
+      throw new Error('invalid corner: ' + stickerMove.corner);
+    }
+  }
 };
 
-StickerMove.prototype._applyMoveToTip = function(stickerMove) {
+Stickers.prototype._applyMoveToTip = function(stickerMove) {
   var front = 0;
   var bottom = 9;
   var right = 18;
@@ -39,12 +94,16 @@ StickerMove.prototype._applyMoveToTip = function(stickerMove) {
     switch (stickerMove.corner) {
     case 0: // r
       this._threeCycle(front+8, right+4, bottom+4);
+      break;
     case 1: // l
       this._threeCycle(front+4, bottom, left+8);
+      break;
     case 2: // u
       this._threeCycle(front, left, right);
+      break;
     case 3: // b
       this._threeCycle(left+4, bottom+8, right+8);
+      break;
     default:
       throw new Error('invalid corner: ' + stickerMove.corner);
     }
@@ -52,30 +111,31 @@ StickerMove.prototype._applyMoveToTip = function(stickerMove) {
     switch (stickerMove.corner) {
     case 0: // r'
       this._reverseThreeCycle(front+8, right+4, bottom+4);
+      break;
     case 1: // l'
       this._reverseThreeCycle(front+4, bottom, left+8);
+      break;
     case 2: // u'
       this._reverseThreeCycle(front, left, right);
+      break;
     case 3: // b'
       this._reverseThreeCycle(left+4, bottom+8, right+8);
+      break;
     default:
       throw new Error('invalid corner: ' + stickerMove.corner);
     }
   }
 };
 
-StickerMove.prototype._threeCycle = function(idx1, idx2, idx3) {
+Stickers.prototype._threeCycle = function(idx1, idx2, idx3) {
   var temp = this._stickers[idx3];
   this._stickers[idx3] = this._stickers[idx2];
   this._stickers[idx2] = this._stickers[idx1];
   this._stickers[idx1] = temp;
 };
 
-StickerMove.prototype._reverseThreeCycle = function(idx1, idx2, idx3) {
-  // NOTE: Inverting a three-cycle is the same thing as doing it twice.
-  for (var i = 0; i < 2; ++i) {
-    this._threeCycle(idx1, idx2, idx3);
-  }
+Stickers.prototype._reverseThreeCycle = function(idx1, idx2, idx3) {
+  this._threeCycle(idx3, idx2, idx1);
 };
 
 exports.Stickers = Stickers;
